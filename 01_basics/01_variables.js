39c8f37e33f45a1d2b2d3011ee7c2b106c7faf4c
Aniket <142729831+aniketkr-mahato@users.noreply.github.com>
2023-12-09 11:05:02 +0000

    basics>01variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 152364
+let accountEmail = "aniketkumar@gmail.com"
+var accountPassword = "125695"
+accountCity = "Delhi"
+let accountState;
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+/*
+prefer using 'let' instead of 'var' for variables
+because of issue in block scope and fuctional scope */
