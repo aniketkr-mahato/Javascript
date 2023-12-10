7805fc4e39898dbb7d60cfbd0cd19f59ac3fa453
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2023-12-10 11:08:28 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treats all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Aniket" //string
+let age = 20 //number
+let isLoggedIn = true //boolean
+
+// number range => 2^53
+// biging is used for bigger numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+
+// object
+
+console.log("aniket");
+console.log(typeof "aniket");
+
+console.log(typeof null);
+// null is an object
