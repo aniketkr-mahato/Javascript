e1711e00087b012efcfe33da15c5fcf8eb0e9583
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2024-01-16 10:16:22 +0530

    added promises

--- 11_Callbacks,PromisesAndAsyncAwait/01_script.js
+++ 11_Callbacks,PromisesAndAsyncAwait/01_script.js
@@ -17,6 +17,22 @@
     -Nested callbacks stacked below one another forming a pyramid structure.(Pyramid of Doom)
 */
 
+function getData(data, getNextData) {
+
+    setTimeout(() => {
+        console.log("data ", data);  //prints data 1 then data 2
+        if (getNextData) {
+            getNextData();
+        }
+    }, 2000);
+}
+
+getData(1, () => {
+    getData(2, () => {
+        getData(3);
+    });
+});
+
 
 /* PROMISES -->
     -Promise is for "eventual" completion of task. It is an object in JS.
@@ -25,4 +41,21 @@
     let promise = new Promise((resolve, reject) => {...}) //Function with 2 handlers
 
     *resolve & reject are callbacks provided by JS.
+*/
+
+
+let promise = new Promise((resolve, reject) => {
+    console.log("I am a new promise.");
+    resolve("promise resolved.");
+});
+/*
+Promise {<fulfilled>: promise resolved.}
+    [[Prototype]]: Promise
+    [[PromiseState]]: "fullfilled"  -->three states: pending, fullfilled and rejected
+    [[PromiseResult]]: "promise resolved."
+
+    A JS Promise object can be:
+    Pending: the result is undefined
+    Resolved: the result is a value (fulfilled)  // resolve(result)
+    Rejected: the result is an error object  // reject(error)
 */
