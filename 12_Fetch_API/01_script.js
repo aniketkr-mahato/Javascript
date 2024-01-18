ef70f2fc3a1a2c024c6c3c0ee0beeed4dc0eeac1
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2024-01-19 00:43:49 +0530

    ...

--- 12_Fetch_API/01_script.js
+++ 12_Fetch_API/01_script.js
@@ -17,12 +17,26 @@ const factPara = document.querySelector("#fact");
 const btn = document.querySelector("#btn");
 
 
-const getFacts = async () => {
-    console.log("getting data.....");
-    let response = await fetch(URL);
-    console.log(response); // JSON format
-    let data = await response.json(); // JSON to JS Object
-    factPara.innerText = data[0].text;
+// const getFacts = async () => {
+//     console.log("getting data.....");
+//     let response = await fetch(URL);
+//     console.log(response); // JSON format
+//     let data = await response.json(); // JSON to JS Object
+//     factPara.innerText = data[0].text;
+// }
+
+function getFacts() {
+    fetch(URL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            factPara.innerText = data[2].text;
+        });
 }
 
 btn.addEventListener("click", getFacts);
+
+
+
